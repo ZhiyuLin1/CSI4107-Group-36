649461a9c6b62f6e80ca0bd461987e78e6d6b526
ZhiyuLin1 <zlin080@uottawa.ca>
2025-02-03 16:57:19 -0500

    Create preprocessing.py

---
+++ A1/preprocessing.py
@@ -0,0 +1,5 @@
+import re
+from nltk.tokenize import word_tokenize
+STOP_WORDS = { "a", "about", "above", "ac", "according", "accordingly",
+               "across","actually", "ad", "adj", "af", "after", "afterwards",
+               "again", "against", "al"}
